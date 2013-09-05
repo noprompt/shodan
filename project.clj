dfacfebf85b84c7f9dea2c3d29ba6375337cbba4
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-05 13:44:13 -0700

    Bump version to v0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject shodan "0.1.0-SNAPSHOT"
+(defproject shodan "0.1.0"
   :description "ClojureScript wrapper for the JavaScript console API."
   :url "https://github.com/noprompt/shodan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
