ed6019bfc36a99fd8242ddf9d5ff1712f0fc459c
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-05-23 16:25:32 -0700

    Return the value of body from console.macros

--- src/shodan/console.clj
+++ src/shodan/console.clj
@@ -1,26 +1,41 @@
 (ns shodan.console
   (:refer-clojure :exclude [time]))
 
-(defmacro with-group [title & body]
+(defmacro with-group
+  "Evaluate body within a new console group with title. Returns the
+  value of body."
+  [title & body]
   `(do
      (.group js/console ~title)
-     ~@body
-     (.groupEnd js/console)))
+     (let [result# (do ~@body)]
+       (.groupEnd js/console)
+       result#)))
  
-(defmacro with-group-collapsed [title & body]
+(defmacro with-group-collapsed
+  "Evaluate body within a new console group (collapsed) with
+  title. Returns the value of body."
+  [title & body]
   `(do
      (.groupCollapsed js/console ~title)
-     ~@body
-     (.groupEnd js/console)))
+     (let [result# ~@body]
+       (.groupEnd js/console)
+       result#)))
  
-(defmacro with-profile [title & body]
+(defmacro with-profile
+  "Evaluate body within a new console profile with title. Returns the
+  value of body."
+  [title & body]
   `(do
      (.profile js/console ~title)
-     ~@body
-     (.profileEnd js/console)))
+     (let [result# (do ~@body)]
+       (.profileEnd js/console)
+       result#)))
  
-(defmacro time [& body]
+(defmacro time
+  "Evaluate body and time it's execution. Returns the value of body."
+  [& body]
   `(do
      (.time js/console "Elapsed time")
-     ~@body
-     (.timeEnd js/console "Elapsed time")))
+     (let [result# (do ~@body)]
+       (.timeEnd js/console "Elapsed time") 
+       result#)))
