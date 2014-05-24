8372378fe76e953238d942664bbf52b5bb8e19f9
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-05-23 20:54:33 -0700

    Include polyfill

--- src/shodan/console.cljs
+++ src/shodan/console.cljs
@@ -1,8 +1,44 @@
 (ns shodan.console
   (:refer-clojure :exclude [assert])
-  (:require [shodan.util :refer [node-js? js-apply]]))
+  (:require [shodan.util :refer [js-apply]]))
 
-;;----------------------------------------------------------------------
+;; ---------------------------------------------------------------------
+;; Polyfill
+
+(let [methods ["assert"
+               "clear"
+               "count"
+               "debug"
+               "dir"
+               "dirxml"
+               "error"
+               "exception"
+               "group"
+               "groupCollapsed"
+               "groupEnd"
+               "info"
+               "log"
+               "markTimeline"
+               "profile"
+               "profileEnd"
+               "table"
+               "time"
+               "timeEnd"
+               "timeStamp"
+               "trace"
+               "warn"]
+      console (if (exists? js/console)
+                js/console
+                ;; IE <= 8
+                (let [polyfill #js {:memory nil}]
+                  (aset js/window "console" polyfill)
+                  polyfill))
+      noop (constantly nil)]
+  (doseq [m methods]
+    (or (aget console m)
+        (aset console m noop))))
+
+;; ---------------------------------------------------------------------
 ;; Logging
 
 (defn log
@@ -10,11 +46,10 @@
   [& args]
   (js-apply (.-log js/console) js/console args))
 
-(when-not node-js?
-  (defn debug
-    "Like `log` but marks the output as debugging information."
-    [& args]
-    (js-apply (.-debug js/console) js/console args)))
+(defn debug
+  "Like `log` but marks the output as debugging information."
+  [& args]
+  (js-apply (.-debug js/console) js/console args))
 
 (defn info
   "Like `log` but marks the output as an informative message."
@@ -34,21 +69,20 @@
 ;;----------------------------------------------------------------------
 ;; Message grouping (browser only)
 
-(when-not node-js?
-  (defn group-start
-    "Begin a console message group."
-    [& args]
-    (js-apply (.-group js/console) js/console args))
+(defn group-start
+  "Begin a console message group."
+  [& args]
+  (js-apply (.-group js/console) js/console args))
 
-  (defn group-start-collapsed
-    "Begin a console message group in a collapsed state."
-    [& args]
-    (js-apply (.-groupCollapsed js/console) js/console args))
+(defn group-start-collapsed
+  "Begin a console message group in a collapsed state."
+  [& args]
+  (js-apply (.-groupCollapsed js/console) js/console args))
 
-  (defn group-end
-    "End a console message group."
-    []
-    (.groupEnd js/console)))
+(defn group-end
+  "End a console message group."
+  []
+  (.groupEnd js/console))
 
 ;;----------------------------------------------------------------------
 ;; Assertion
@@ -62,9 +96,8 @@
 (defn dir [obj]
   (.dir js/console obj))
 
-(when-not node-js?
-  (defn dirxml [node]
-    (.dirxml js/console node)))
+(defn dirxml [node]
+  (.dirxml js/console node))
 
 (defn trace []
   (.trace js/console))
@@ -72,16 +105,15 @@
 ;;----------------------------------------------------------------------
 ;; Profiling (browser only)
 
-(when-not node-js?
-  (defn profile-start
-    "Begin a profile named title."
-    [title]
-    (.profile js/console title))
+(defn profile-start
+  "Begin a profile named title."
+  [title]
+  (.profile js/console title))
 
-  (defn profile-end
-    "End a profile."
-    []
-    (.profileEnd js/console)))
+(defn profile-end
+  "End a profile."
+  []
+  (.profileEnd js/console))
 
 ;;----------------------------------------------------------------------
 ;; Timing
