e1eab8b418e22fe872912a67bc996603c4c1bd55
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-07-08 19:44:05 -0500

    inspect function to pretty-print data structures to console

---
+++ src/shodan/inspect.cljs
@@ -0,0 +1,40 @@
+(ns shodan.inspect
+  (:require
+    [shodan.console :as console :include-macros true]))
+
+(defn- find-type [obj]
+  (cond
+    (map? obj) :map
+    (vector? obj) :vector
+    (seq? obj) :seq
+    :else :default))
+
+(defmulti inspect
+  "Inspects a ClojureScript data structure by displaying it in the
+  browser's console. Collections are shown using log groups to
+  represent their structure."
+  (fn [x & args] (find-type x)))
+
+(defmethod inspect :map [a-map prefix]
+  (console/with-group "{"
+    (doseq [[k v] a-map]
+      (inspect v (pr-str k)))
+    (console/log "}")))
+
+(defmethod inspect :vector [a-vec prefix]
+  (console/with-group "["
+    (doseq [obj a-vec]
+      (inspect obj))
+    (console/log "]")))
+
+(defmethod inspect :seq [a-vec prefix]
+  (console/with-group "("
+    (doseq [obj a-vec]
+      (inspect obj))
+    (console/log ")")))
+
+(defmethod inspect :default
+  ([obj]
+   (console/log (pr-str obj)))
+  ([obj prefix]
+   (console/log (or prefix "") (pr-str obj))))
