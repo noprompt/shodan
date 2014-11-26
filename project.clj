5ae4613f34f9b5a0aca56f647b760437e212c1ac
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-11-25 22:15:29 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,20 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
-  [[org.clojure/clojure "1.6.0" :scope "provided"]
-   [org.clojure/clojurescript "0.0-2371" :scope "provided"]
-   [com.andrewmcveigh/cljs-time "0.1.6"]]
+  [[org.clojure/clojure "1.6.0"]]
 
   :profiles
   {:dev
    {:dependencies
-    [[com.cemerick/piggieback "0.1.3"]
-     [figwheel "0.1.4-SNAPSHOT"]
+    [[org.clojure/clojurescript "0.0-2371"]
+     [com.cemerick/piggieback "0.1.3"]
      [weasel "0.4.2"]]
 
     :plugins
-    [[lein-figwheel "0.1.4-SNAPSHOT"]
-     [lein-cljsbuild "1.0.3"]]
+    [[lein-cljsbuild "1.0.3"]]
 
     :source-paths
     ["src" "dev"]
@@ -27,7 +24,7 @@
     {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 
   :cljsbuild
-  {:builds [{:id "shodan"
+  {:builds [{:id "dev"
              :source-paths ["src" "dev"]
              :compiler {:output-to "resources/public/shodan.js"
                         :output-dir "resources/public/out"
