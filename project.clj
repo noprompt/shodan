d595afc39396cd00199d3ae86a2af60ba8de24d4
Joel Holdbrooks <cjholdbrooks@gmail.com>
2015-05-17 10:17:37 -0700

    Set development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shodan "0.4.2"
+(defproject shodan "0.4.3-SNAPSHOT"
   :description "ClojureScript wrapper for the JavaScript console API."
   :url "https://github.com/noprompt/shodan"
   :license {:name "Eclipse Public License"
