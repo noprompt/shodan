a0ee7b432eec4010162d97c6c59bfa8c0eb92ae8
Joel Holdbrooks <cjholdbrooks@gmail.com>
2015-05-17 10:16:24 -0700

    Version bump to 0.4.2

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ A ClojureScript library providing wrappers for the JavaScript
 Add Shodan as a `:dependency` in your `project.clj` file.
 
 ```clojure
-[shodan "0.4.1"]
+[shodan "0.4.2"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shodan "0.4.2-SNAPSHOT"
+(defproject shodan "0.4.2"
   :description "ClojureScript wrapper for the JavaScript console API."
   :url "https://github.com/noprompt/shodan"
   :license {:name "Eclipse Public License"
